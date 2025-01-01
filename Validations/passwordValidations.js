bae0e246c8cdfcaad1412f7defc71c66db62b78f
kriparajp1 <kripp.me@outlook.com>
2025-01-01 15:36:51 +0530

    new validation update

--- Validations/passwordValidations.js
+++ Validations/passwordValidations.js
@@ -23,5 +23,18 @@ const pass = (password) => {
     if (typeof minLength !== "number" || minLength <= 0) return { valid: false, reason: "Minimum length must be a positive number." };
     return input.length >= minLength ? { valid: true } : { valid: false, reason: "Input does not meet minimum length." };
   };
+
+    /**
+   * Checks for password and confirm password match.
+   * @param {string} str - String to validate.
+   * @returns {Object} - { valid: boolean, reason?: string }
+   */
+function match(input1, input2) {
+  if (input1 === input2) {
+      return { valid: true };
+  } else {
+      return { valid: false, reason: "Password does not match!" };
+  }
+}
   
-  module.exports = { pass, minLen };
+  module.exports = { pass, minLen,match };
--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const passwordValidations = require('./Validations/passwordValidations');
 const numberValidations = require('./Validations/numberValidations');
 const textCaseValidations = require('./Validations/textCaseValidations');
 const whiteSpaceValidations = require("./Validations/whiteSpaceValidation");
-const MatchValidation = require("./Validations/MatchValidation");
+
 
 module.exports = {
   ...basicValidations,
   ...passwordValidations,
   ...numberValidations,
   ...textCaseValidations,
-  ...whiteSpaceValidations,
-  ...MatchValidation
+  ...whiteSpaceValidations
 };
